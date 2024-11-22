51e1c4720e5ac1cb0573f4ad520e3379c53dca53
InsightpulseIP <Whitesangalang@gmail.com>
2024-11-22 12:41:08 +0800

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -4,12 +4,3 @@ const slider = document.querySelector('.slider');
 function updateSlider() {
     slider.style.transform = `translateX(-${currentIndex * 100}%)`; 
 }
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex === 0) ? cards.length - 1 : currentIndex - 1;
-    updateSlider();
-});
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex === cards.length - 1) ? 0 : currentIndex + 1;
-    updateSlider();
-});
